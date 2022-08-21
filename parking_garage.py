24ce3d57865d383bacfcce17cc5bfa446519431c
James <leeja328@gmail.com>
2022-08-21 10:29:17 -0500

    final

--- parking_garage.py
+++ parking_garage.py
@@ -7,103 +7,103 @@ class Parking:
         self.tickets = tickets
         self.parking_spaces = parking_spaces
         self.current_ticket = {}
-        self.take_ticket()
-
-
-    def take_ticket(self):
-        while True:
-            welcome = input("Do you want to park in the garage or are you leaving? (park/leave/quit)")
-            if welcome.lower() == 'quit':
-                break
-
-            elif welcome.lower() == 'park':
-                self.pay_for_parking()
-                
-
-            else: 
-                welcome.lower() == 'leave'
-                self.leave_garage()
-                break
+       
  
-            
-
-
-    # def p(self):
-    #     print(self.tickets)
-    #     print(self.parking_spaces)
-    #     print(self.current_ticket)
-
 
     def pay_for_parking(self):
-        # while True:
-            if self.tickets == 0:
-                print("There are no spaces in this garage, go elsewhere! \nThe passcode to get out is 123")
-                # break
-                self.take_ticket()
+        if self.tickets == 0:
+            print("There are no spaces in this garage, go elsewhere! \nThe passcode to get out is 123")
+            
+        else:
+            hours = int(input("How many hours did you park with us? "))
+            if hours <= 1:
+                self.current_ticket[self.tickets] = True
+                print(f"Your ticket # is {self.tickets}.")
+                self.tickets -= 1
+                self.parking_spaces -= 1
+                print(f"There are {self.parking_spaces} parking spaces left.")
+                print(f"Ticket costs $6. You have 1 hrs to leave.")
+                
+
+            elif hours <= 2:
+                self.current_ticket[self.tickets] = True
+                print(f"Your ticket # is {self.tickets}.")
+                self.tickets -= 1
+                self.parking_spaces -= 1
+                print(f"There are {self.parking_spaces} parking spaces left.")
+                print(f"Ticket costs $8. You have 2 hrs to leave.")
+                
+
+            elif hours <= 4:
+                self.current_ticket[self.tickets] = True
+                print(f"Your ticket # is {self.tickets}.")
+                self.tickets -= 1
+                self.parking_spaces -= 1
+                print(f"There are {self.parking_spaces} parking spaces left.")
+                print(f"Ticket costs $10. You have 4 hrs to leave.")
+                
+
+            elif hours <= 12:
+                self.current_ticket[self.tickets] = True
+                print(f"Your ticket # is {self.tickets}.")
+                self.tickets -= 1
+                self.parking_spaces -= 1
+                print(f"There are {self.parking_spaces} parking spaces left.")
+                print(f"Ticket costs $16. You have 12 hrs to leave.")
+                
+            elif hours <= 24:
+                self.current_ticket[self.tickets] = True
+                print(f"Your ticket # is {self.tickets}.")
+                self.tickets -= 1
+                self.parking_spaces -= 1
+                print(f"There are {self.parking_spaces} parking spaces left.")
+                print(f"Ticket costs $20. You have 24 hrs to leave.")
+
             else:
-                hours = int(input("How many hours did you park with us? "))
-                if hours <= 1:
-                    self.current_ticket[self.tickets] = True
-                    print(f"Your ticket # is {self.tickets}.")
-                    self.tickets -= 1
-                    self.parking_spaces -= 1
-                    print(f"There are {self.parking_spaces} parking spaces left.")
-                    print(f"Ticket costs $6. You have {hours} hrs to leave.")
-                    self.take_ticket()
-
-                elif hours <= 2:
-                    self.current_ticket[self.tickets] = True
-                    self.tickets -= 1
-                    self.parking_spaces -= 1
-                    print(f"Ticket costs $8. You have {hours} hrs to leave. Your ticket # is {self.tickets}")
-                    self.leave_garage
-
-                elif hours <= 4:
-                    self.current_ticket[self.tickets] = True
-                    self.tickets -= 1
-                    self.parking_spaces -= 1
-                    print(f"Ticket costs $10. You have {hours} hrs to leave. Your ticket # is {self.tickets}")
-                    self.leave_garage
-
-                elif hours <= 12:
-                    self.current_ticket[self.tickets] = True
-                    self.tickets -= 1
-                    self.parking_spaces -= 1
-                    print(f"Ticket costs $16. You have {hours} hrs to leave. Your ticket # is {self.tickets}")
-                    self.leave_garage
-                    
-                elif hours <= 24:
-                    self.current_ticket[self.tickets] = True
-                    self.tickets -= 1
-                    self.parking_spaces -= 1
-                    print(f"Ticket costs $20. You have {hours} hrs to leave. Your ticket # is {self.tickets}")
-                    self.leave_garage
-
-                else:
-                    self.current_ticket[self.tickets] = False
-                    print("Ticket has not been paid. Enter how many hrs you want to park for...")
-                    self.pay_for_parking
-                    self.leave_garage()
+                self.current_ticket[self.tickets] = False
+                print("You can only park here for a max 24 hours.")
+                print("Please, enter valid response.")
+                self.pay_for_parking
         
         
     def leave_garage(self):
-        leave = int(input("What is your ticket #? if no ticket enter passcode: "))
+        leave = int(input("What is your ticket number? If no ticket enter passcode: "))
         
         if leave in self.current_ticket:
             if self.current_ticket[leave] == True:
                 self.tickets += 1
                 self.parking_spaces += 1
                 print("Thank you have a nice day!")
-                self.take_ticket()
+                
 
         elif leave == 123:
             print("Sorry you couldn't park here, come another day!")
 
         else:
-            print("Looks like you still need to pay! >:(")
-            self.pay_for_parking()
+            print("Ticket or passcode not recognized, please try again ")
+            self.leave_garage
             return
 
 
 Garage = Parking(10,10)
 
+
+def take_ticket():
+        while True:
+            response = input("Do you want to park in the garage or are you leaving? (park/leave/quit)")
+            if response.lower().strip() == 'quit':
+                break
+
+            elif response.lower().strip() == 'park':
+                Garage.pay_for_parking()
+                    
+
+            elif response.lower().strip() == 'leave':
+                Garage.leave_garage()
+                
+            
+            else:
+                print("That is not a valid response! Please pick one from the list")
+
+
+take_ticket()
---
+++ readme.txt
@@ -0,0 +1,9 @@
+James:
+This was a joint effort in that we live streamed the vscode
+and worked on it simultaneously.
+I was able to fix the looping issue with a run() method.
+
+
+Cecilia:
+I was able to correct some logic errors and fix some of the wording!
+I was also able to do some debugging as well.
